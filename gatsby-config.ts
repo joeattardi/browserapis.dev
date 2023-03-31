353e92f5abc5e29a86a8a24bb24949acb1e3e281
Joe Attardi <jattardi@gmail.com>
2023-03-30 22:21:51 -0400

    initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'Web Browser API Cookbook',
+    siteUrl: 'https://browserapis.dev'
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
