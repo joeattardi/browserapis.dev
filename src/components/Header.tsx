c77363648a4663caf7472ef73a385858595a9038
Joe Attardi <jattardi@gmail.com>
2023-04-02 01:38:45 -0400

    add some icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
-import { header } from './Layout.css';
+import { MdOutlineRestaurant } from 'react-icons/md';
+import { header, logoIcon } from './Layout.css';
 
 const Header = () => {
   return (
     <header className={header}>
+      <MdOutlineRestaurant size={32} className={logoIcon} />
       Web Browser API Cookbook
     </header>
   )
--- src/components/Layout.css.ts
+++ src/components/Layout.css.ts
@@ -1,4 +1,4 @@
-import { style } from '@vanilla-extract/css';
+import { globalStyle, style } from '@vanilla-extract/css';
 import { vars } from '../theme.css';
 
 export const layout = style({
@@ -13,7 +13,13 @@ export const header = style({
   background: vars.color.header.background,
   color: vars.color.header.text,
   fontSize: vars.font.size.lg,
-  padding: vars.spacing.md
+  padding: vars.spacing.lg,
+  display: 'flex',
+  alignItems: 'center'
+});
+
+export const logoIcon = style({
+  marginRight: vars.spacing.md
 });
 
 export const main = style({
