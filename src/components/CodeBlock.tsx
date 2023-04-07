b592048775d43bb92427331832d32639f39323d7
Joe Attardi <jattardi@gmail.com>
2023-04-06 23:22:37 -0400

    Migrate code blocks

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,49 +1,51 @@
 import React, { useState } from 'react';
 import clsx from 'clsx';
-import dracula from 'prism-react-renderer/themes/dracula';
-import Highlight, { defaultProps } from 'prism-react-renderer';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { MdContentCopy, MdCheck } from 'react-icons/md';
 
-import { codeBlock, tag, lang, toolbar, copyButton, copySuccess } from './CodeBlock.css';
+import { codeBlock, copyButton, toolbar, html, css, javascript } from './CodeBlock.module.scss';
+
+const languageClass = {
+  javascript,
+  html,
+  css
+};
 
 const languageTags = {
   javascript: 'JS'
-}
+};
 
 export default function CodeBlock({ language, code }) {
   const [copied, setCopied] = useState(false);
 
   function onCopy() {
     setCopied(true);
-
     setTimeout(() => setCopied(false), 5000);
   }
+
+  const Icon = copied ? MdCheck : MdContentCopy;
 
   return (
     <div className={codeBlock}>
       <div className={toolbar}>
-        <div className={clsx(tag, lang[language])}>{languageTags[language] || language.toUpperCase()}</div>
+        <div className={clsx('tag is-medium', languageClass[language])}>{languageTags[language] || language.toUpperCase()}</div>
         <CopyToClipboard text={code} onCopy={onCopy}>
-          <button className={clsx(copyButton, copied && copySuccess)}>
-            {copied ? <MdCheck /> : <MdContentCopy />}
+          <button className={clsx('button', copyButton, copied && 'is-success is-light')}>
+            <Icon className="mr-2" />
             {copied ? 'Copied!' : 'Copy'}
           </button>
         </CopyToClipboard>
       </div>
-      <Highlight {...defaultProps} code={code} language={language} theme={dracula}>
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
+      <SyntaxHighlighter
+        customStyle={{ fontSize: '1rem' }} 
+        language={language} 
+        style={dracula}
+      >
+        {code}
+      </SyntaxHighlighter>
     </div>
   );
 }
