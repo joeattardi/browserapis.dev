14421eb6bb8d0334c2a2e35563367845a34f3b71
Joe Attardi <jattardi@gmail.com>
2023-04-12 23:38:29 -0400

    Fix theme for real

---
+++ gatsby-ssr.js
@@ -0,0 +1,22 @@
+const React = require('react');
+
+// Apply the correct theme before rendering to avoid a flash of the wrong theme.
+exports.onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+  (function() {
+    const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const cachedTheme = sessionStorage.getItem('theme');
+    if (cachedTheme != null) {
+      document.documentElement.className = cachedTheme;
+    } else {
+      document.documentElement.className = darkQuery.matches ? 'dark' : 'light';
+    }
+  })();
+  `,
+      }}
+    />,
+  ]);
+};
