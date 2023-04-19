648c45eac7716cd8bf15b4e1388e7e50ea57d54f
Joe Attardi <jattardi@gmail.com>
2023-04-19 06:54:29 -0400

    fix twitter SEO

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -3,7 +3,6 @@ import { useStaticQuery, graphql } from 'gatsby';
 import useSiteMetadata from '../hooks/useSiteMetadata';
 
 export default function SEO(props) {
-  console.log({ props });
   const { pageTitle: localPageTitle, pageContext, description, location } = props;
   const { title: siteTitle, description: defaultDescription, siteUrl, image, twitterUsername } = useSiteMetadata();
   const pageTitle = pageContext?.frontmatter?.title || localPageTitle;
@@ -14,7 +13,8 @@ export default function SEO(props) {
     <>
       <html lang="en" />
       <meta name="description" content={pageDescription} />
-      <meta name="twitter:card" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content={image} />
       <meta name="twitter:description" content={pageDescription} />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={title} />
